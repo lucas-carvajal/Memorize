d711d90c5cbead9ee5f106a9e7fc7b5200ee5f75
Lucas <lucas.carvajal@stud.uni-bamberg.de>
2021-02-18 14:50:01 +0100

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Lucas Carvajal on 18.02.21.
+//  Copyright © 2021 Lucas Carvajal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
